23f755f0367be1e855e0b1d9ddd5b3177cdfcfd4
Jonyelson Lopes <jonyelson20@gmail.com>
2023-01-24 00:08:11 -0300

    first commit

---
+++ src/main/java/com/tecodev/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.tecodev.productservice.controller;
+
+import com.tecodev.productservice.dto.ProductRequest;
+import com.tecodev.productservice.dto.ProductResponse;
+import com.tecodev.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ src/main/java/com/tecodev/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tecodev.productservice.repository;
+
+import com.tecodev.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
